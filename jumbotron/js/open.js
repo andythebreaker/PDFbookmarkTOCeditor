e2b55276d8a8ce16e615bee4a1c4c2351079b85e
andythebreaker <e24074724@mail.ncku.edu.tw>
2021-10-10 00:49:52 +0800

    時間表更新

--- jumbotron/js/open.js
+++ jumbotron/js/open.js
@@ -16,26 +16,43 @@ var currentTimeMinutes = currentDate.getMinutes();
 var timeNow = currentTimeHours + "" + currentTimeMinutes;
 
 var currentDayID = "#" + currentDay; //gets todays weekday and turns it into id
-$(currentDayID).toggleClass("today"); //this works at hightlighting today
+$(currentDayID).toggleClass("active"); //this works at hightlighting today
 
-var openTimeSplit = $(currentDayID).children('.opens').text().split(":");
-
-var openTimeHours = openTimeSplit[0];
-openTimeHours = openTimeHours < 10 ? "0" + openTimeHours : openTimeHours;
-
-var openTimeMinutes = openTimeSplit[1];
-var openTimex = openTimeSplit[0] + openTimeSplit[1];
-
-var closeTimeSplit = $(currentDayID).children('.closes').text().split(":");
-
-var closeTimeHours = closeTimeSplit[0];
-closeTimeHours = closeTimeHours < 10 ? "0" + closeTimeHours : closeTimeHours;
-
-var closeTimeMinutes = closeTimeSplit[1];
-var closeTimex = closeTimeSplit[0] + closeTimeSplit[1];
-
-if (timeNow >= openTimex && timeNow <= closeTimex) {
-    $(".openorclosed").toggleClass("open");
-} else {
+if ($(currentDayID).children('.no_right_border.no_left_border').hasClass('shutdown')) {
+    //this day is colse!!!
     $(".openorclosed").toggleClass("closed");
+    //$("#open-status").toggleClass("negative");
+
+    //TODO:Exception handling
+    $(currentDayID).children('.no_right_border.no_left_border').text(' ');
+    $(currentDayID).children('.opens').text($(
+        '.openinghours .openinghourscontent .OpeningHoursChineseNameFormDefinitionCollection .shutdownName1'
+    ).text());
+    $(currentDayID).children('.closes').text($(
+        '.openinghours .openinghourscontent .OpeningHoursChineseNameFormDefinitionCollection .shutdownName2'
+    ).text());
+} else {
+    var openTimeSplit = $(currentDayID).children('.opens').text().split(":");
+
+    var openTimeHours = openTimeSplit[0];
+    openTimeHours = openTimeHours < 10 ? "0" + openTimeHours : openTimeHours;
+
+    var openTimeMinutes = openTimeSplit[1];
+    var openTimex = openTimeSplit[0] + openTimeSplit[1];
+
+    var closeTimeSplit = $(currentDayID).children('.closes').text().split(":");
+
+    var closeTimeHours = closeTimeSplit[0];
+    closeTimeHours = closeTimeHours < 10 ? "0" + closeTimeHours : closeTimeHours;
+
+    var closeTimeMinutes = closeTimeSplit[1];
+    var closeTimex = closeTimeSplit[0] + closeTimeSplit[1];
+
+    if (timeNow >= openTimex && timeNow <= closeTimex) {
+        $(".openorclosed").toggleClass("open");
+        //$("#open-status").toggleClass("positive");
+    } else {
+        $(".openorclosed").toggleClass("closed");
+        //$("#open-status").toggleClass("negative");
+    }
 }
